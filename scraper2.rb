18e44c913485348bc3c6c450d47a33fb95cb2170
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2026-04-23 23:47:20 -0300

    dfg

--- scraper2.rb
+++ scraper2.rb
@@ -117,24 +117,136 @@ def scrape_optioncarriere(browser, city, title_query)
 end
 #BIENVENUE SUR THEJUNGLE.COM
 #
-def scrape_welcometothejungle(...)
+def scrape_welcometothejungle(browser, city, title_query)
+  site_name = "welcome to the jungle"
+  browser.goto "https://www.welcometothejungle/"
+  browser.text_field(id: 'search-query-field').set(title_query+" "+city) rescue nil
+  browser.button(type: 'submit').click
+
+  browser.wait_until { |b| b.div(css: '[data-pagetype="jobs_index"]').exists? rescue true }
+
+  browser.elements(css: '[data-pagetype="jobs_index"]').each do |card|
+    job_title   = card.element(css: 'h2 a').text rescue nil
+    job_snippet = card.element(css: '.desc').text[0..300] rescue nil
+    job_url     = card.element(css: 'h2 a').href rescue nil
+    next if job_title.nil? || job_title.empty?
+
+    save_job(
+      site: site_name,
+      city: city,
+      title_query: title_query,
+      job_title: job_title,
+      job_snippet: job_snippet,
+      job_url: job_url
+    )
+  end
 end
 #JOOBLE.COM
 #
-def scrape_jooble(...)
+def scrape_jooble(browser, city, title_query)
+  site_name = "welcome to the jungle"
+  browser.goto "https://www.welcometothejungle/"
+  browser.text_field(id: 'input_:R55j8h:').set(title_query) rescue nil
+  browser.text_field(id: 'tbRegion').set(city) rescue nil
+  browser.button(type: 'submit').click
+
+  browser.wait_until { |b| b.div(css: '[data-test-name="_jobCard"]').exists? rescue true }
+
+  browser.elements(css: '[data-test-name="_jobCard"]').each do |card|
+    job_title   = card.element(css: '.job_card_link a').text rescue nil
+    job_snippet = card.element(css: 'span').text[0..300] rescue nil
+    job_url     = card.element(css: '.job_card_link a').href rescue nil
+    next if job_title.nil? || job_title.empty?
+
+    save_job(
+      site: site_name,
+      city: city,
+      title_query: title_query,
+      job_title: job_title,
+      job_snippet: job_snippet,
+      job_url: job_url
+    )
+  end
 end
 #JOBIJOBA.COM
 #
-def scrape_jobijoba(...)
+def scrape_jobijoba(browser, city, title_query)
+  site_name = "jobi joba"
+  browser.goto "https://www.jobijoba.com/"
+  browser.text_field(css: '#form_what_input input').set(title_query) rescue nil
+  browser.text_field(css: '#form_where_input input').set(city) rescue nil
+  browser.button(type: 'submit').click
+
+  browser.wait_until { |b| b.div(css: '.offer').exists? rescue true }
+
+  browser.elements(css: '.offer').each do |card|
+    job_title   = card.element(css: '.offer-header-title').text rescue nil
+    job_snippet = card.element(css: '.description').text[0..300] rescue nil
+    job_url     = card.element(css: 'a').href rescue nil
+    next if job_title.nil? || job_title.empty?
+
+    save_job(
+      site: site_name,
+      city: city,
+      title_query: title_query,
+      job_title: job_title,
+      job_snippet: job_snippet,
+      job_url: job_url
+    )
+  end
 end
 #BLADA.COM
 #
 #CHOISIRLESERVICEPUBLIC.GOUV.FR (pour postuler aux offres de service public avec ou sans concurrence)
-def scrape_choisirleservicepublic(...)
+def scrape_choisirleservicepublic(browser, city, title_query)
+  site_name = "choisir le service public"
+  browser.goto "https://choisirleservicepublic.gouv.fr"
+  browser.text_field(css: '#input').set(title_query+" "+city) rescue nil
+  browser.button(type: 'submit').click
+
+  browser.wait_until { |b| b.div(css: '.fr-card--offer').exists? rescue true }
+
+  browser.elements(css: '.fr-card--offer').each do |card|
+    job_title   = card.element(css: '.is-same-domain').text rescue nil
+    job_snippet = card.element(css: '.fr-icon-calendar-line.fr-icon--sm').text[0..300] rescue nil
+    job_url     = card.element(css: 'a.is-same-domain').href rescue nil
+    next if job_title.nil? || job_title.empty?
+
+    save_job(
+      site: site_name,
+      city: city,
+      title_query: title_query,
+      job_title: job_title,
+      job_snippet: job_snippet,
+      job_url: job_url
+    )
+  end
 end
 #
 #RECRUTEMENT.EDUCATION.GOUV.FR
-def scrape_recrutementeducationgouv(...)
+def scrape_recrutementeducationgouv(browser, city, title_query)
+  site_name = "jobi joba"
+  browser.goto "https://recrutement.education.gouv.fr/"
+  browser.text_field(css: '#SearchTerm-67').set(title_query+" "+city) rescue nil
+  browser.button(type: 'submit').click
+
+  browser.wait_until { |b| b.div(css: '.fr-card.fr-enlarge-link.fr-card--horizontal.fr-card--horizontal-tier.fr-card--sm').exists? rescue true }
+
+  browser.elements(css: '.fr-card.fr-enlarge-link.fr-card--horizontal.fr-card--horizontal-tier.fr-card--sm').each do |card|
+    job_title   = card.element(css: '.fr-card__title').text rescue nil
+    job_snippet = card.element(css: '.fr-card__desc').text[0..300] rescue nil
+    job_url     = card.element(css: 'a').href rescue nil
+    next if job_title.nil? || job_title.empty?
+
+    save_job(
+      site: site_name,
+      city: city,
+      title_query: title_query,
+      job_title: job_title,
+      job_snippet: job_snippet,
+      job_url: job_url
+    )
+  end
 end
 #
 #(pour postuler aux offres de l'Education Nationale)
@@ -144,45 +256,45 @@ end
 #(pour postuler à des offres en hôtellerie ou restauration)
 #
 #JOB-INTERIM-GUYANE.FR
-def scrape_jobinterimguyane(...)
+def scrape_jobinterimguyane(browser, city, title_query)
 end
 #
 #ANTILLES-GUYANE.FIDERIM.FR
-def scrape_antillesguyanefiderim(...)
+def scrape_antillesguyanefiderim(browser, city, title_query)
 end
 #
 #FOMATGUYANE.FR
-def scrape_fomatguyane(...)
+def scrape_fomatguyane(browser, city, title_query)
 end
 #
 #GROUPEACTUAL.EU
-def scrape_groupeactual(...)
+def scrape_groupeactual(browser, city, title_query)
 end
 #
 #ADECCO.FR
-def scrape_adecco(...)
+def scrape_adecco(browser, city, title_query)
 end
 #
 #EMPLOIS.INCLUSION.BETA.GOUV.FR/RECHERCHE/EMPLOYEURS (trouver un emploi dans notre Structure d'Activité Economique d'Insertion)
-def scrape_emploisinclusion(...)
+def scrape_emploisinclusion(browser, city, title_query)
 end
 #
 #IMMERSION-FACILE.BETA.GOUV.FR (trouver une entreprise pour réaliser une immersion professionnelle)
-def scrape_immersionfacile(...)
+def scrape_immersionfacile(browser, city, title_query)
 end
 #
 #Et aussi les réseaux sociaux :
 #
 #FR.LINKEDIN.COM
-def scrape_linkedin(...)
+def scrape_linkedin(browser, city, title_query)
 end
 #
 #FACEBOOK.COM, TIKTOK.COM, INSTAGRAM.COM
-def scrape_facebook(...)
+def scrape_facebook(browser, city, title_query)
 end
-def scrape_tiktok(...)
+def scrape_tiktok(browser, city, title_query)
 end
-def scrape_linkedin(...)
+def scrape_linkedin(browser, city, title_query)
 end
 
 # Tu pourras ajouter d’autres méthodes :
